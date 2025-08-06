b79780810986cc664547d07086a768aa9ae0489d
zeinabsh <84870536+zeinabsh@users.noreply.github.com>
2025-08-06 17:11:40 +0300

    Add files via upload
    Bike Rental Analysis with Power BI & SQL 📊
    
    In this project, I performed a detailed analysis of bike rental data for 2021 & 2022 using SQL Server,  and built a fully interactive dashboard with Power BI to extract actionable insights.
    
    🛠️ Key Steps:
    🔄 Data Merging & Cleaning:
    Combined multiple tables using UNION.
    
    Joined a pricing table to calculate Revenue and Profit.
    
    Renamed columns for clarity.
    
    Changed data types where needed for proper modeling in Power BI.
    
    ⚙️ SQL Data Transformation:
    Categorized hours into time periods: Morning, Afternoon, Evening, Night.
    
    Grouped weekdays vs. weekends.
    
    Classified holidays, seasons, weather conditions, and rider types.
    
    Calculated key metrics:
    
    Revenue = Riders × Price
    
    Profit = Revenue – (Riders × COGS)
    
    📊 Power BI Dashboard Development:
    KPIs: Total Riders, Profit, Revenue.
    
    Visual insights based on:
    
    Revenue by Holiday status, Season, and Time of day.
    
    Rider Category (Registered vs Casual).
    
    Monthly and yearly performance trends.
    
    💡 Key Insights:
    Revenue in 2022 reached $10.2M, compared to $5M in 2021.
    
    Total rider count: 3 million.
    
    Registered riders made up the majority of total users.
    
    Profit increases with ride volume — especially during:
    
    Morning, Afternoon, and Evening on Weekdays.
    
    Non-holiday days generate significantly more revenue.
    
    Summer is the most profitable season.
    
    🎯 Tools & Tech Used:
    Power BI, SQL Server, Data Modeling, Data Transformation, Data Visualization

---
+++ bike rental/bike.sql
@@ -0,0 +1,92 @@
+﻿
+WITH bike AS (
+    SELECT * FROM bike_share_yr_0
+    UNION 
+    SELECT * FROM bike_share_yr_1
+)
+
+SELECT 
+    -- Divide the date into year and month
+   CASE A.yr
+        WHEN 0 THEN 2021
+        WHEN 1 THEN 2022
+    END AS ride_year,
+
+    MONTH(CONVERT(date, A.dteday, 103)) AS ride_month,
+
+    A.hr AS hour,
+
+    -- Classify the hour into a time period
+    CASE 
+        WHEN A.hr BETWEEN 0 AND 5 THEN 'Night'
+        WHEN A.hr BETWEEN 6 AND 11 THEN 'Morning'
+        WHEN A.hr BETWEEN 12 AND 17 THEN 'Afternoon'
+        WHEN A.hr BETWEEN 18 AND 23 THEN 'Evening'
+        ELSE 'Unknown'
+    END AS time_period,
+
+    --  holiday
+    CASE A.holiday
+        WHEN 1 THEN 'Holiday'
+        ELSE 'Non-Holiday'
+    END AS holiday_status,
+
+    -- day of the week 
+    CASE A.weekday
+        WHEN 0 THEN 'Sunday'
+        WHEN 1 THEN 'Monday'
+        WHEN 2 THEN 'Tuesday'
+        WHEN 3 THEN 'Wednesday'
+        WHEN 4 THEN 'Thursday'
+        WHEN 5 THEN 'Friday'
+        WHEN 6 THEN 'Saturday'
+    END AS day_of_week,
+
+    --  "Weekend" أو "Weekday"
+    CASE A.weekday
+        WHEN 0 THEN 'Weekend'
+        WHEN 6 THEN 'Weekend'
+        ELSE 'Weekday'
+    END AS week_type,
+
+    -- season
+    CASE A.season
+        WHEN 1 THEN 'Winter'
+        WHEN 2 THEN 'Spring'
+        WHEN 3 THEN 'Summer'
+        WHEN 4 THEN 'Fall'
+    END AS season,
+
+    -- weather 
+    CASE A.weathersit
+        WHEN 1 THEN 'Clear'
+        WHEN 2 THEN 'Cloudy'
+        WHEN 3 THEN 'Rainy'
+        WHEN 4 THEN 'Stormy'
+    END AS weather,
+
+   -- Edit column names
+    A.temp AS temperature,
+    A.atemp AS feels_like_temp,
+    A.hum AS humidity,
+    A.windspeed AS wind_speed,
+
+    A.rider_type AS rider_category,
+    A.riders AS rider_count,
+
+    B.price AS ride_price,
+    B.COGS AS ride_cost,
+
+    -- revenue and profit
+    (A.riders * B.price) AS revenue,
+    (A.riders * B.price - A.riders * B.COGS) AS profit
+
+
+	
+
+	FROM 
+    bike A
+LEFT JOIN 
+    cost_table B
+ON 
+    A.yr = B.yr
